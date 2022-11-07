fe9ee0afd50cd8a3fd21cfcd219ddee037531f41
theCarlG <me@carlg.tech>
2022-11-07 17:54:32 +0100

    fix(nvim): use nightly rust-analyzer
    Signed-off-by: theCarlG <me@carlg.tech>

--- common/config/nvim/lua/CarlG/lsp.lua
+++ common/config/nvim/lua/CarlG/lsp.lua
@@ -64,7 +64,7 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 )
 
 nvim_lsp.rust_analyzer.setup({
-    cmd = { "rustup", "run", "stable", "rust-analyzer" },
+    cmd = { "rustup", "run", "nightly", "rust-analyzer" },
     capabilities=capabilities,
     on_attach = on_attach,
     settings = {
@@ -81,7 +81,7 @@ nvim_lsp.rust_analyzer.setup({
                 enable = true,
             },
             checkOnSave = {
-                command = "cranky",
+                command = "clippy",
             },
         }
     }
@@ -137,24 +137,6 @@ nvim_lsp.clangd.setup {
     }
 }
 
-function OrgImports(wait_ms)
-    local params = vim.lsp.util.make_range_params()
-    params.context = {only = {"source.organizeImports"}}
-    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
-    if result then
-        for _, res in pairs(result or {}) do
-            if res.result then
-                for _, r in pairs(res.result or {}) do
-                    if r.edit then
-                        vim.lsp.util.apply_workspace_edit(r.edit)
-                    else
-                        vim.lsp.buf.execute_command(r.command)
-                    end
-                end
-            end
-        end
-    end
-end
 function go_org_imports(wait_ms)
     vim.lsp.buf.formatting()
     local params = vim.lsp.util.make_range_params()
@@ -178,4 +160,3 @@ vim.api.nvim_command("au BufEnter *.go setlocal noexpandtab")
 vim.api.nvim_command("au BufEnter *.c setlocal expandtab tabstop=2 softtabstop=2 shiftwidth=2")
 vim.api.nvim_command("au BufEnter *.h setlocal expandtab tabstop=2 softtabstop=2 shiftwidth=2")
 vim.api.nvim_command("au BufWritePre *.go lua go_org_imports(1000)")
-
