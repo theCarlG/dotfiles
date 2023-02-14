2fbf52cec6410f22440d8fdeca247979bd2d25b6
theCarlG <me@carlg.tech>
2023-02-14 18:25:53 +0100

    nvim: tailwindcss lsp

--- common/config/nvim/lua/CarlG/lsp.lua
+++ common/config/nvim/lua/CarlG/lsp.lua
@@ -105,6 +105,39 @@ lsp.configure('gopls', {
     },
 })
 
+local root_pattern = require('lspconfig.util').root_pattern
+lsp.configure('tailwindcss', {
+    filetypes = {
+        "css",
+        "scss",
+        "sass",
+        "postcss",
+        "html",
+        "javascript",
+        "javascriptreact",
+        "typescript",
+        "typescriptreact",
+        "svelte",
+        "vue",
+        "rust",
+        "rs"
+    },
+    experimental = {
+        classRegex = {
+            [[class="([^"]*)]],
+            "class=\\s+\"([^\"]*)"
+        },
+    },
+    init_options = {  
+        userLanguages = {  
+            eelixir = "html-eex",  
+            eruby = "erb",  
+            rust = "html",  
+        },  
+    },
+    root_dir = root_pattern('tailwind.config.js', 'tailwind.config.ts', 'postcss.config.js',
+    'postcss.config.ts', 'package.json', 'node_modules'),
+})
 
 lsp.setup()
 
