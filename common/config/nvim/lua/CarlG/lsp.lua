00547fe6bdb7d41cdfc0d4e36466135e04cf27c2
theCarlG <carl.gunnarsson@embark-studios.com>
2023-06-28 15:32:38 +0200

    nvim: nvim-cmp mapping

--- common/config/nvim/lua/CarlG/lsp.lua
+++ common/config/nvim/lua/CarlG/lsp.lua
@@ -21,7 +21,19 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
   ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+  ['<CR>'] = cmp.mapping(function(fallback)
+      -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+      if cmp.visible() then
+          local entry = cmp.get_selected_entry()
+          if not entry then
+              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+          else
+              cmp.confirm()
+          end
+      else
+          fallback()
+      end
+  end, {"i","s","c",})
 })
 
 -- disable completion with tab
