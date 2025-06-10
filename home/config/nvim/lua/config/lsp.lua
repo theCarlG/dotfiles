907ab0334f7b48b54ec44fa1a7b818ffd1cd1240
theCarlG <me@carlg.tech>
2025-06-10 14:58:17 +0200

    nvim

--- home/config/nvim/lua/config/lsp.lua
+++ home/config/nvim/lua/config/lsp.lua
@@ -27,9 +27,8 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     callback = function()
         local mode = vim.api.nvim_get_mode().mode
         local filetype = vim.bo.filetype
-        if vim.bo.modified == true and mode == 'n' and filetype ~= "oil" then
+        if vim.bo.modified == true and mode == 'n' and filetype ~= "oil" and filetype ~= "cpp" and filetype ~= "c" then
             vim.cmd('lua vim.lsp.buf.format()')
-        else
         end
     end
 })
