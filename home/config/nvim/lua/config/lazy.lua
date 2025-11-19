c0e2145c337dea44bc1288ee697f7265fead83be
Carl Gunnarsson <carl.broberg-gunnarsson@teliacompany.com>
2025-11-19 09:38:28 +0100

    nvim: fix nvim tmux navigation in terminal

--- home/config/nvim/lua/config/lazy.lua
+++ home/config/nvim/lua/config/lazy.lua
@@ -18,7 +18,34 @@ require("lazy").setup({
     spec = {
         { import = "plugins" },
         --[[ Misc ]]
-        { 'christoomey/vim-tmux-navigator' },
+        {
+            'christoomey/vim-tmux-navigator',
+            cmd = {
+                "TmuxNavigateLeft",
+                "TmuxNavigateDown",
+                "TmuxNavigateUp",
+                "TmuxNavigateRight",
+                "TmuxNavigatePrevious",
+                "TmuxNavigatorProcessList",
+            },
+            init = function()
+                -- Reusable function to register keymaps in different contexts
+                local function set_keymaps()
+                    vim.keymap.set({ "n", "t" }, "<C-h>", "<cmd>TmuxNavigateLeft<cr>")
+                    vim.keymap.set({ "n", "t" }, "<C-j>", "<cmd>TmuxNavigateDown<cr>")
+                    vim.keymap.set({ "n", "t" }, "<C-k>", "<cmd>TmuxNavigateUp<cr>")
+                    vim.keymap.set({ "n", "t" }, "<C-l>", "<cmd>TmuxNavigateRight<cr>")
+                end
+
+                -- Register once globally
+                set_keymaps()
+
+                -- Re-register for terminal buffers to prevent literal command injection
+                vim.api.nvim_create_autocmd("TermOpen", {
+                    callback = set_keymaps,
+                })
+            end,
+        },
         { 'mg979/vim-visual-multi' },
         {
             'rmagatti/auto-session',
